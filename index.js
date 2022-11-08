8fec268e1192c131043671c388ef7cdc8776c672
mrc4619 <mrchayon5525@gmail.com>
2022-11-08 10:16:19 +0600

    mongodb setup

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
@@ -10,6 +11,24 @@ app.use(cors());
 app.use(express.json());
 
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.w79fzld.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+
+    const servicesCollection = client.db('exPhotographyDB').collection('servicesDB');
+
+    app.get('/services', async (req, res) => {
+        const query = {};
+        const cursor = servicesCollection.find(query);
+        const services = await cursor.toArray();
+        res.send(services);
+    })
+
+}
+
+run().catch(error => console.error(error))
 
 app.get('/', (req, res) => {
     res.send('Photography website server running');
