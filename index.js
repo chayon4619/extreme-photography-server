410573485dd809e0a41ac479c04de7130f651ec9
mrc4619 <mrchayon5525@gmail.com>
2022-11-08 15:10:48 +0600

    delete method added

--- index.js
+++ index.js
@@ -66,6 +66,13 @@ async function run() {
         res.send(result)
     });
 
+    app.delete('/review/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await reviewCollection.deleteOne(query);
+        res.send(result);
+    })
+
 }
 
 run().catch(error => console.error(error))
